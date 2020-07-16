13768da03a03cc23ea0967ae26e171dda0e2a4da
Pedro Felipe Gomez Bonilla <pipe22007@Pedros-MacBook-Pro.local>
2020-07-16 18:10:46 -0500

    Initial start of Scanner

--- src/edu/upb/ds/Main.java
+++ src/edu/upb/ds/Main.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("\n############################");
+        System.out.println("\nWelcome to the Provider Panel Control");
+        System.out.println("\nPlease Select an option:");
 
     }
 }
