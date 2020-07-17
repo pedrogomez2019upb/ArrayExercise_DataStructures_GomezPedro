ed0b4ec0a13d37edb0cb873b86fa55d3b7f6a125
Pedro Felipe Gomez Bonilla <pipe22007@Pedros-MacBook-Pro.local>
2020-07-17 00:22:29 -0500

    Attributes ,Get-Set & Constructor created

--- src/edu/upb/ds/Provider.java
+++ src/edu/upb/ds/Provider.java
@@ -1,4 +1,39 @@
 package edu.upb.ds;
 
 public class Provider {
+    private String name;
+    private String city;
+    private double numArticles;
+
+    public Provider(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public double getNumArticles() {
+        return numArticles;
+    }
+
+    public void setNumArticles(double numArticles) {
+        this.numArticles = numArticles;
+    }
+
+    public Provider(String name, String city, double numArticles) {
+        this.name = name;
+        this.city = city;
+        this.numArticles = numArticles;
+    }
 }
