a4675d9a4b34fae0763a1780a917eba4655f49f0
Pedro Felipe Gomez Bonilla <pipe22007@Pedros-MacBook-Pro.local>
2020-07-17 00:32:37 -0500

    Creating Menu

--- src/edu/upb/ds/Main.java
+++ src/edu/upb/ds/Main.java
@@ -5,10 +5,16 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        int option=0;
         Scanner sc= new Scanner(System.in);
         System.out.println("\n############################");
         System.out.println("\nWelcome to the Provider Panel Control");
+        System.out.println("\n############################");
         System.out.println("\nPlease Select an option:");
-
+        System.out.println("\n1:See Provider:");
+        System.out.println("\n2:Change city name;");
+        System.out.println("\n3:Create Provider:");
+        System.out.println("\n4:Delete Provider");
+        option=sc.nextInt();
     }
 }
